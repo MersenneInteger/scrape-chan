1480518abe91c22357b9e2e2932758e32ff487e4
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-07-31 21:29:06 -0500

    init - build proj

---
+++ ScrapeChan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeChan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var os = Environment.OSVersion;
+
+            if (os.Platform.Equals("Win32NT"))
+            {
+                //
+            }
+            else if (os.Platform.Equals("MacOSX"))
+            {
+                //
+            }
+            else if (os.Platform.Equals("Unix"))
+            {
+                //
+            }
+        }
+    }
+}
