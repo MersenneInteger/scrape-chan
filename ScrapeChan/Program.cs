04fd3912eab8d26ab431ac247cb5691556323339
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-08-01 09:57:37 -0500

    use RuntimeInformation.IsOsPlatform for os info, add test path, add Scraper class

--- ScrapeChan/Program.cs
+++ ScrapeChan/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +13,53 @@ namespace ScrapeChan
     {
         static void Main(string[] args)
         {
-            var os = Environment.OSVersion;
+            //<a class="fileThumb" href=""
+            Scraper scraper = new Scraper();
+            string url = "https://is2.4chan.org/wg/1562245792289.png";
+            string savePath = string.Empty;
 
-            if (os.Platform.Equals("Win32NT"))
+            savePath = CreateDirectoryToSavePictures();
+
+            if (url.Contains("https"))
+                url.Replace("https", "http");
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadFile(new Uri(url), $"{savePath}\\image35.png");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// determine default path to create new directory in, return path
+        /// </summary>
+        /// <returns>String</returns>
+        private static string CreateDirectoryToSavePictures()
+        {
+            var savePath = string.Empty;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                savePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                savePath += @"\chanPics\";
+                if (!Directory.Exists(savePath))
+                    Directory.CreateDirectory(savePath);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 //
             }
-            else if (os.Platform.Equals("MacOSX"))
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 //
             }
-            else if (os.Platform.Equals("Unix"))
-            {
-                //
-            }
+            return savePath;
         }
     }
 }
